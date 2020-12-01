00996d8c5a2c8a6da49eed569940acf35c213b96
Amirhossein <ahshadravan@gmail.com>
2020-12-01 18:20:16 +0330

    ex1 added

--- Week3/js-exercises/ex1-AddSix.js
+++ Week3/js-exercises/ex1-AddSix.js
@@ -1,7 +1,7 @@
 /**
- 
+
  ** Exercise 1: Add Six **
- 
+
 Declare a function called `createBase`.The function takes a number as a parameter and
 return a closure, that adds a number to the base number argument.
 
@@ -10,10 +10,18 @@ Call the function three times. The return values should be:
 
  */
 
-function createBase( /* ???? */ ) {
+function createBase(a) {
   // Put here your logic...
+  return function(b){
+
+    return console.log(a+b);
+  }
 }
 
 const addSix = createBase(6);
 
 // Put here your function calls...
+
+addSix(9);
+addSix(18);
+addSix(27);
