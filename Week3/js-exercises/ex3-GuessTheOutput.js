20268d458180f8f59b3a3b9a31bb00ea02941fea
Amirhossein <ahshadravan@gmail.com>
2020-12-02 17:00:06 +0330

    ex3 is added

--- Week3/js-exercises/ex3-GuessTheOutput.js
+++ Week3/js-exercises/ex3-GuessTheOutput.js
@@ -3,11 +3,11 @@
 ** Exercise 3: Guess the output **
 
 Look at the bellow code snippet.
-Can you guess the output? 
+Can you guess the output?
 Write out your reasoning in 50 words or less.
 
 */
-
+// At first it gave a the amount of 10 but in the x variable again it gave a the amount of 12. So the alert function is looking for the first amount of a and that would be 12. Also when we used let to identify the a variable 12 will replace 10.
 
 
 let a = 10;
